7dac6cf7fcc56f390a6a9a0660d72485e192f2af
Jonathan <exrazhell@yahoo.fr>
2023-05-04 10:08:06 +0200

    [REFACTO] suppression magic numbers

--- src/jpeg2ppm.c
+++ src/jpeg2ppm.c
@@ -3,6 +3,24 @@
 #include <string.h>
 #include <math.h>
 
+
+#define THREE_BYTES_LONG 3
+
+#define SEGMENT_START 0xff  // Segment start marker
+#define SOI 0xd8            // Start of Image
+#define SOF_0 0xc0          // Baseline DCT
+#define SOF_1 0xc1          // Extended sequential DCT
+#define SOF_2 0xc2          // Progressive DCT
+#define SOF_3 0xc3          // Lossless (sequential)
+#define DHT 0xc4            // Define Huffman Table(s)
+#define DQT 0xdb            // Define Quantization Table(s)
+#define SOS 0xda            // Start of Scan
+#define EOI 0xd9            // End of Image
+
+#define LUMINANCE_ID 0x00
+#define CHROMINANCE_ID 0x01
+
+
 short size_seg(FILE *input){
     short length = 0;
     short length2 = 0;
@@ -46,9 +64,9 @@ int main(int argc, char **argv) {
     }
     
     // Vérification JPEG Magic number 
-    unsigned char first3bytes[3];
+    unsigned char first3bytes[THREE_BYTES_LONG];
     fread(first3bytes, sizeof(first3bytes), 1, input);
-    unsigned char JPEG_magic_Number[3] = {0xff, 0xd8, 0xff};
+    unsigned char JPEG_magic_Number[THREE_BYTES_LONG] = {SEGMENT_START, SOI, SEGMENT_START};
 
     for (int i=0; i<3; i++){
         if (first3bytes[i] != JPEG_magic_Number[i]){
@@ -63,16 +81,16 @@ int main(int argc, char **argv) {
 
     while (1){
         fread(buffer, 1, 1, input);
-        if (buffer[0] == 0xff){
+        if (buffer[0] == SEGMENT_START){
             fread(id, 1, 1, input);
-            if (id[0] == 0xdb){
+            if (id[0] == DQT){
                 printf("Quantization table\n");
                 short length = 0;
                 length = size_seg(input);
                 length = length - 2 - 1;
 
                 fread(buffer, 1, 1, input);
-                if (buffer[0] == 0x00) {
+                if (buffer[0] == LUMINANCE_ID) {
                     unsigned char *quantization_table_luminance = malloc(length*sizeof(unsigned char));
                     fread(quantization_table_luminance, length, 1, input);
                     // Affichage des tables de quantification
@@ -80,7 +98,7 @@ int main(int argc, char **argv) {
                         printf("%x", quantization_table_luminance[i]);
                     }
                     printf("\n");
-                } else if (buffer[0] == 0x01) {
+                } else if (buffer[0] == CHROMINANCE_ID) {
                     unsigned char *quantization_table_chrominance = malloc(length*sizeof(unsigned char));
                     fread(quantization_table_chrominance, length, 1, input);
                     // Affichage des tables de quantification
@@ -93,7 +111,7 @@ int main(int argc, char **argv) {
                     return EXIT_FAILURE;
                 }
 
-            } else if (id[0] == 0xc0){
+            } else if (id[0] == SOF_0){
                 printf("Start of frame\n");
                 short length = 0;
                 length = size_seg(input);
@@ -152,13 +170,13 @@ int main(int argc, char **argv) {
                     printf("Facteur d'échantillonnage Y : %d\n", sampling_factor_y);
                 }
 
-            } else if (id[0] == 0xc4){
+            } else if (id[0] == DHT){
                 printf("Huffman table\n");
 
-            } else if (id[0] == 0xda){
+            } else if (id[0] == SOS){
                 printf("Start of scan + data\n");
 
-            } else if (id[0] == 0xd9){
+            } else if (id[0] == EOI){
                 printf("Fin du fichier\n");
                 break;
             }
