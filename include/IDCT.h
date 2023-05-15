7362de91e747356dce695cb7c9a30ca7a19b9cc8
Jonathan <exrazhell@yahoo.fr>
2023-05-15 15:43:45 +0200

    [FIX] IDCT

--- include/IDCT.h
+++ include/IDCT.h
@@ -4,6 +4,8 @@
 #include <math.h>
 #include <stdio.h>
 
+#include <extract.h>
+#include <IQ.h>
 #include <utils.h>
 
 #define PI 3.14159265358979323846
@@ -16,6 +18,8 @@ void initialize();
 void load();
 
 // Inverse Discrete Cosine Transform function
-int * idct(int input[NN]);
+int8_t IDCT_function(struct JPEG *jpeg,size_t MCU_number, int8_t component_index);
+
+int8_t IDCT(struct JPEG * jpeg);
 
 #endif
--- src/IDCT.c
+++ src/IDCT.c
@@ -31,6 +31,7 @@ void initialize() {
     fclose(file);
 }
 
+
 void load() {
     FILE *file;
 
@@ -39,22 +40,71 @@ void load() {
     fclose(file);
 }
 
-int * idct(int input[NN]) {
-    int *output = (int *) malloc(NN * sizeof(int));
-    for (int x = 0; x < N; x++) {
-        for (int y = 0; y < N; y++) { 
+
+// Inverse Discrete Cosine Transform function
+int8_t IDCT_function(struct JPEG *jpeg, size_t MCU_number, int8_t component_index){
+
+    int16_t** MCUs = get_MCUs(get_sos_component(get_sos_components(get_JPEG_sos(jpeg)[0]), component_index));
+
+    initialize();
+    load();
+    
+    int16_t *output = (int16_t *) malloc(NN * sizeof(int16_t));
+    if (check_memory_allocation(output)) return EXIT_FAILURE;
+
+    for (int8_t x = 0; x < N; x++) {
+        for (int8_t y = 0; y < N; y++) { 
             double sum = 0.0;
 
-            for (int u = 0; u < N; u++) {
-                for (int v = 0; v < N; v++) {
-                    int dct_uv = input[u * N + v];
+            for (int8_t u = 0; u < N; u++) {
+                for (int8_t v = 0; v < N; v++) {
+                    int16_t dct_uv = MCUs[MCU_number][u * N + v];
                     sum +=  dct_uv * C_cos_values[x][y][u][v];
                 }
             }
-            output[x * N + y] = round(sum);
+
+            if (round(sum) + 128 < 0) {
+                output[x * N + y] = 0;
+            } else if (round(sum) + 128 > 255) {
+                output[x * N + y] = 255;
+            } else {
+                output[x * N + y] = round(sum) + 128;
+            }
         }
     }
 
-    return output;
+    print_block(MCUs[MCU_number]);
+    free(MCUs[MCU_number]);
+    MCUs[MCU_number] = output;
+    print_block(MCUs[MCU_number]);
+
+    return EXIT_SUCCESS;
 }
 
+
+int8_t IDCT(struct JPEG * jpeg) {
+    size_t nb_mcu_width = 0;
+    size_t nb_mcu_height = 0;
+    if (get_JPEG_width(jpeg) % 8 == 0) {
+        nb_mcu_width =  get_JPEG_width(jpeg) / 8;
+    } else {
+        nb_mcu_width = (get_JPEG_width(jpeg) / 8) + 1;
+    }
+    if (get_JPEG_width(jpeg) % 8 == 0) {
+        nb_mcu_height =  get_JPEG_width(jpeg) / 8;
+    } else {
+        nb_mcu_height = (get_JPEG_width(jpeg) / 8) + 1;
+    }
+
+    // On parcours tous les MCUs de l'image
+    for (size_t i = 0; i < nb_mcu_width * nb_mcu_height; i++){
+        // Prévoir possibilité de reset-er les données `previous_DC_values` dans le cas où l'on a
+        // plusieurs scans/frames ---> mode progressif
+        
+        // On parcours toutes les composantes
+        for (int8_t j = 0; j < get_sos_nb_components(get_JPEG_sos(jpeg)[0]); ++j) {   // attention ici l'index 0 correspond au 1er scan/frame ... prévoir d'intégrer un index pour le mode progressif
+            if (IDCT_function(jpeg, i, j) ) return EXIT_FAILURE;
+        }
+    }
+    return EXIT_SUCCESS;
+}
