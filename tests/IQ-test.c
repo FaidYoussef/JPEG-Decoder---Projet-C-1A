0b92b30c1ab6c95bbe9654ce941d71ba7e66ff80
Jonathan <exrazhell@yahoo.fr>
2023-05-18 01:46:53 +0200

    [TESTS] IQ & IZZ reconstruits suite aux dernières modifications ...

---
+++ tests/IQ-test.c
@@ -0,0 +1,175 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <IQ.h>
+#include <utils.h>
+#include <verbose.h>
+
+bool highly_verbose = true;
+
+
+const uint8_t test_qt0[64] = {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0
+};
+
+const uint8_t test_qt1[64] = {
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1
+};
+
+const uint8_t test_qt2[64] = {
+    2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2
+};
+
+const uint8_t test_qt1and2[64] = {
+    1, 2, 1, 2, 1, 2, 1, 2,
+    2, 1, 2, 1, 2, 1, 2, 1,
+    1, 2, 1, 2, 1, 2, 1, 2,
+    2, 1, 2, 1, 2, 1, 2, 1,
+    1, 2, 1, 2, 1, 2, 1, 2,
+    2, 1, 2, 1, 2, 1, 2, 1,
+    1, 2, 1, 2, 1, 2, 1, 2,
+    2, 1, 2, 1, 2, 1, 2, 1
+};
+
+
+const uint8_t test_qt255[64] = {
+    255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 255, 255, 255, 255, 255
+};
+
+void re_initialize_data(int16_t data[64]){
+    for (int i = 0; i < 64; i++) {
+        data[i] = 1;
+    }
+}
+
+
+
+// tests inverse quantization
+int main(void) {
+
+    int16_t initial_data[64] = {
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1
+    };
+
+    //*************************************************************************************************
+    // test 1 : on multiplie par 0
+    getHighlyVerbose() ? fprintf(stderr, "MCU avant IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+    IQ(initial_data, test_qt0);
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU après IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+
+    //*************************************************************************************************
+    // test 2 : on multiplie par 1
+    re_initialize_data(initial_data);
+
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU avant IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+    IQ(initial_data, test_qt1);
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU après IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+
+    //*************************************************************************************************
+    // test 3 : on multiplie par 2
+    re_initialize_data(initial_data);
+
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU avant IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+    IQ(initial_data, test_qt2);
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU après IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+
+    //*************************************************************************************************
+    // test 4 : on multiplie par 1 et par 2
+    re_initialize_data(initial_data);
+
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU avant IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+    IQ(initial_data, test_qt1and2);
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU après IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+
+    //*************************************************************************************************
+    // test 5 : on multiplie par 255
+    re_initialize_data(initial_data);
+
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU avant IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+    IQ(initial_data, test_qt255);
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU après IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+
+    //*************************************************************************************************
+    // test 6 : on multiplie par 255 et on évite les overflows
+    re_initialize_data(initial_data);
+    initial_data[0] = -32768;
+    initial_data[63] = 32767;
+
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU avant IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+    IQ(initial_data, test_qt255);
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU après IQ\n"):0;
+    print_block(initial_data, 0, 0);
+
+
+
+    return EXIT_SUCCESS;
+}
---
+++ tests/IZZ-test.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <IZZ.h>
+#include <utils.h>
+#include <verbose.h>
+
+
+bool highly_verbose = true;
+
+
+// tests inverse zig_zag
+int main(void) {
+
+    int16_t initial_data[64] = {
+        0, 1, 8, 16, 9, 2, 3, 10,
+        17, 24, 32, 25, 18, 11, 4, 5,
+        12, 19, 26, 33, 40, 48, 41, 34,
+        27, 20, 13, 6, 7, 14, 21, 28,
+        35, 42, 49, 56, 57, 50, 43, 36,
+        29, 22, 15, 23, 30, 37, 44, 51,
+        58, 59, 52, 45, 38, 31, 39, 46,
+        53, 60, 61, 54, 47, 55, 62, 63
+    };
+
+    int16_t expected_data[64] = {
+        0, 1, 2, 3, 4, 5, 6, 7,
+        8, 9,10,11,12,13,14,15,
+        16,17,18,19,20,21,22,23,
+        24,25,26,27,28,29,30,31,
+        32,33,34,35,36,37,38,39,
+        40,41,42,43,44,45,46,47,
+        48,49,50,51,52,53,54,55,
+        56,57,58,59,60,61,62,63
+    };
+
+    //*************************************************************************************************
+    // test 1 : on dé_zig-zag
+    getHighlyVerbose() ? fprintf(stderr, "MCU avant IZZ\n"):0;
+    print_block(initial_data, 0, 0);
+
+    IZZ_function((int16_t **) &initial_data);
+
+    getHighlyVerbose() ? fprintf(stderr, "MCU après IZZ\n"):0;
+    print_block(initial_data, 0, 0);
+
+    bool result = true;
+    for(int i = 0; i < 64; i++){
+        if(initial_data[i] != expected_data[i]) result = false;
+    }
+    result ? fprintf(stderr, "test : OK\n") : fprintf(stderr, "test : KO !!!\n");
+
+
+    return EXIT_SUCCESS;
+}
