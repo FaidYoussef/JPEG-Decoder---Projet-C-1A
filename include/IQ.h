90e68155acb923cfae6a3a07d993ab6412a3094b
Jonathan <exrazhell@yahoo.fr>
2023-05-17 15:57:37 +0200

    [FIX] spécification fonctions IQ remaniées pour tests

--- include/IQ.h
+++ include/IQ.h
@@ -13,7 +13,10 @@
 // Fonction qui permet de savoir si on a un overflow
 bool isOverflow(int16_t a, int16_t b);
 
-// Inverse quantization function using quant_table
+// Inverse quantization function
+void IQ(int16_t *mcu, const uint8_t *qtable);
+
+// Fonction qui récupère les données de la structure JPEG et qui procède à la quantification inverse
 int8_t inv_quantize(struct JPEG * jpeg);
 
 #endif
--- src/IQ.c
+++ src/IQ.c
@@ -15,8 +15,21 @@ bool isOverflow(int16_t a, int16_t b) {
 }
 
 
-// Inverse quantization function using quant_table
-// qblock : les données qui sont quantifiées
+// Inverse quantization function
+void IQ(int16_t *mcu, const uint8_t *qtable){
+    for (int8_t k = 0; k < 64; k++) {
+        if (isOverflow(mcu[k], qtable[k]) && mcu[k] < 0) {
+            mcu[k] = -32768;
+        } else if (isOverflow(mcu[k], qtable[k]) && mcu[k] > 0) {
+            mcu[k] = 32767;
+        } else {
+            mcu[k] = mcu[k] * qtable[k];
+        }
+    }
+}
+
+
+// Fonction qui récupère les données de la structure JPEG et qui procède à la quantification inverse
 int8_t inv_quantize(struct JPEG * jpeg) {
     size_t nb_mcu_width = 0;
     size_t nb_mcu_height = 0;
@@ -37,22 +50,18 @@ int8_t inv_quantize(struct JPEG * jpeg) {
         int8_t qt_index = get_num_quantization_table(get_sof_components((get_JPEG_sof(jpeg)[0]))[i]);
         getHighlyVerbose() ? fprintf(stderr, "qt_index : %d\n", qt_index):0;
         struct QuantizationTable *qt = get_JPEG_qt(jpeg)[qt_index];
-        unsigned char *qt_table = get_qt_data(qt);
+        const uint8_t *qt_table = get_qt_data(qt);
+        
+        // On récupère les MCUs de la composante
         int16_t** MCUs = get_MCUs(get_sos_component(get_sos_components(get_JPEG_sos(jpeg)[0]), i));
 
         // On parcours tous les MCUs de l'image
         for (size_t j = 0; j < nb_mcu_width * nb_mcu_height; j++){
             getHighlyVerbose() ? fprintf(stderr, "MCU avant IQ\n"):0;
             print_block(MCUs[j], j);
-            for (int8_t k = 0; k < 64; k++) {
-                if (isOverflow(MCUs[j][k], qt_table[k]) && MCUs[j][k] < 0) {
-                    MCUs[j][k] = -32768;
-                } else if (isOverflow(MCUs[j][k], qt_table[k]) && MCUs[j][k] > 0) {
-                    MCUs[j][k] = 32767;
-                } else {
-                    MCUs[j][k] = MCUs[j][k] * qt_table[k];
-                }
-            }
+
+            IQ(MCUs[j], qt_table);
+
             getHighlyVerbose() ? fprintf(stderr, "MCU après IQ\n"):0;
             print_block(MCUs[j], j);
         }
