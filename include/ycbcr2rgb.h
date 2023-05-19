1002bf6e0ac678268dc951e26f79c0a5279c03f3
Jonathan <exrazhell@yahoo.fr>
2023-05-19 22:32:36 +0200

    [FIX] généralisation des fonctions dans YCbCr

--- include/ycbcr2rgb.h
+++ include/ycbcr2rgb.h
@@ -14,7 +14,10 @@ uint8_t saturer(int16_t valeur);
 // Fonction pour convertir un pixel YCbCr en pixel RGB
 // Si on a une seule composante, on ne met à jour que la composante 0
 // Si on a plusieurs composantes, on met à jour en lieu et place des composantes 0, 1 et 2 les valeurs R, G et B
-void ycbcr_vers_rgb(struct JPEG *jpeg, size_t MCU_number);
+void pixel_YCbCr2RGB(int16_t *pixel_Y, int16_t *pixel_Cb, int16_t *pixel_Cr, int8_t nb_components);
+
+// Fonction pour convertir un MCU YCbCr en pixel RGB
+void MCU_YCbCr2RGB(int16_t *MCU_Y, int16_t *MCU_Cb, int16_t *MCU_Cr, int8_t nb_components);
 
 int8_t YCbCr2RGB(struct JPEG *jpeg);
 
--- src/ycbcr2rgb.c
+++ src/ycbcr2rgb.c
@@ -12,62 +12,61 @@ uint8_t saturer(int16_t valeur) {
     }
 }
 
-
 // Fonction pour convertir un pixel YCbCr en pixel RGB
 // Si on a une seule composante, on ne met à jour que la composante 0
 // Si on a plusieurs composantes, on met à jour en lieu et place des composantes 0, 1 et 2 les valeurs R, G et B
-void ycbcr_vers_rgb(struct JPEG *jpeg, size_t MCU_number) {
-    int16_t* MCU_component0 = get_MCUs(get_sos_component(get_sos_components(get_JPEG_sos(jpeg)[0]), COMPONENT_0_INDEX))[MCU_number];
-    int16_t* MCU_component1 = NULL;
-    int16_t* MCU_component2 = NULL;
+void pixel_YCbCr2RGB(int16_t *pixel_Y, int16_t *pixel_Cb, int16_t *pixel_Cr, int8_t nb_components) {
 
-    if (get_sos_nb_components(get_JPEG_sos(jpeg)[0]) > 1){
-        MCU_component1 = get_MCUs(get_sos_component(get_sos_components(get_JPEG_sos(jpeg)[0]), COMPONENT_1_INDEX))[MCU_number];
-        MCU_component2 = get_MCUs(get_sos_component(get_sos_components(get_JPEG_sos(jpeg)[0]), COMPONENT_2_INDEX))[MCU_number];
-    }
+        int16_t r = *pixel_Y;
+        int16_t g = *pixel_Y;
+        int16_t b = *pixel_Y;
+
+        if (nb_components > 1){
+            r += 1.402 * (*pixel_Cr - 128);
+            g += - 0.34414 * (*pixel_Cb - 128) - 0.71414 * (*pixel_Cr - 128);
+            b += 1.772 * (*pixel_Cb - 128);
+        }
+
+        *pixel_Y = saturer(r);
+
+        if (nb_components > 1){
+            *pixel_Cb = saturer(g);
+            *pixel_Cr = saturer(b);
+        }
+    
+}
+
+
+// Fonction pour convertir un MCU YCbCr en pixel RGB
+void MCU_YCbCr2RGB(int16_t *MCU_Y, int16_t *MCU_Cb, int16_t *MCU_Cr, int8_t nb_components) {
 
     for (int8_t i = 0; i < 64; i++){
-        int16_t r = MCU_component0[i];
-        int16_t g = MCU_component0[i];
-        int16_t b = MCU_component0[i];
-
-        if (get_sos_nb_components(get_JPEG_sos(jpeg)[0]) > 1){
-            r += 1.402 * (MCU_component2[i] - 128);
-            g += - 0.34414 * (MCU_component1[i] - 128) - 0.71414 * (MCU_component2[i] - 128);
-            b += 1.772 * (MCU_component1[i] - 128);
-        }
-
-        MCU_component0[i] = saturer(r);
-
-        if (get_sos_nb_components(get_JPEG_sos(jpeg)[0]) > 1){
-            MCU_component1[i] = saturer(g);
-            MCU_component2[i] = saturer(b);
-        }
+        pixel_YCbCr2RGB(&MCU_Y[i], &MCU_Cb[i], &MCU_Cr[i], nb_components);
     }
     
 }
 
 
 int8_t YCbCr2RGB(struct JPEG *jpeg){
-    size_t nb_mcu_width = 0;
-    size_t nb_mcu_height = 0;
-    if (get_JPEG_width(jpeg) % 8 == 0) {
-        nb_mcu_width =  get_JPEG_width(jpeg) / 8;
-    } else {
-        nb_mcu_width = (get_JPEG_width(jpeg) / 8) + 1;
-    }
-    if (get_JPEG_height(jpeg) % 8 == 0) {
-        nb_mcu_height =  get_JPEG_height(jpeg) / 8;
-    } else {
-        nb_mcu_height = (get_JPEG_height(jpeg) / 8) + 1;
-    }
+    size_t nb_mcu_width = (get_JPEG_width(jpeg) + 7) / 8;
+    size_t nb_mcu_height = (get_JPEG_height(jpeg) + 7) / 8;
+
 
     // On parcours tous les MCUs de l'image
     for (size_t i = 0; i < nb_mcu_width * nb_mcu_height; i++){
         // Prévoir possibilité de reset-er les données `previous_DC_values` dans le cas où l'on a
         // plusieurs scans/frames ---> mode progressif
 
-        ycbcr_vers_rgb(jpeg, i);
+        int16_t* MCU_Y = get_MCUs(get_sos_component(get_sos_components(get_JPEG_sos(jpeg)[0]), COMPONENT_0_INDEX))[i];
+        int16_t* MCU_Cb = NULL;
+        int16_t* MCU_Cr = NULL;
+
+        if (get_sos_nb_components(get_JPEG_sos(jpeg)[0]) > 1){
+            MCU_Cb = get_MCUs(get_sos_component(get_sos_components(get_JPEG_sos(jpeg)[0]), COMPONENT_1_INDEX))[i];
+            MCU_Cr = get_MCUs(get_sos_component(get_sos_components(get_JPEG_sos(jpeg)[0]), COMPONENT_2_INDEX))[i];
+        }
+
+        MCU_YCbCr2RGB(MCU_Y, MCU_Cb, MCU_Cr, get_sos_nb_components(get_JPEG_sos(jpeg)[0]));
     }
     return EXIT_SUCCESS;
 }
