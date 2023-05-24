ceb4513f969b4125ddf8f822e4a19431a5597744
Jonathan <exrazhell@yahoo.fr>
2023-05-24 02:30:32 +0200

    [QUALITE] déplacement commentaire

--- src/IQ.c
+++ src/IQ.c
@@ -1,7 +1,6 @@
 #include <IQ.h>
 
 
-
 // Inverse quantization function
 void IQ_function(int16_t *mcu, const uint8_t *qtable) {
     for (int8_t k = 0; k < 64; k++) {
@@ -19,10 +18,11 @@ void IQ_function(int16_t *mcu, const uint8_t *qtable) {
 // Fonction qui récupère les données de la structure JPEG et qui procède à la quantification inverse
 int8_t IQ(struct JPEG * jpeg) {
 
-
-    // On parcours tous les MCUs de l'image
+    // On parcourt tous les MCUs de l'image
     for (size_t y = 0; y < get_JPEG_nb_Mcu_Height(jpeg); y += get_JPEG_Sampling_Factor_Y(jpeg)) {
         for (size_t x = 0; x < get_JPEG_nb_Mcu_Width(jpeg); x += get_JPEG_Sampling_Factor_X(jpeg)) {
+
+            // On parcourt toutes les composantes
             for (int8_t i = 0; i < get_sos_nb_components(get_JPEG_sos(jpeg)[0]); i++) {   // attention ici l'index 0 correspond au 1er scan/frame ... prévoir d'intégrer un index pour le mode progressif
                 
                 // On récupère la table de quantification associée à la composante
@@ -31,10 +31,9 @@ int8_t IQ(struct JPEG * jpeg) {
                 struct QuantizationTable *qt = get_JPEG_qt(jpeg)[qt_index];
                 const uint8_t *qt_table = get_qt_data(qt);
                 
-                // On parcours toutes les composantes
                 // On récupère les MCUs de la composante
                 int16_t** MCUs = get_MCUs(get_sos_component(get_sos_components(get_JPEG_sos(jpeg)[0]), i));
-                
+
                 for (int8_t v = 0; v < get_sampling_factor_y(get_sof_component(get_sof_components(get_JPEG_sof(jpeg)[0]), i)); v++) {
                     for (int8_t h = 0; h < get_sampling_factor_x(get_sof_component(get_sof_components(get_JPEG_sof(jpeg)[0]), i)); h++) {
                         // On récupère le MCU
@@ -55,5 +54,3 @@ int8_t IQ(struct JPEG * jpeg) {
     }
     return EXIT_SUCCESS;
 }
-
-
